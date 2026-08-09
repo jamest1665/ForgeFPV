60143b8cff8be122020ab3f139140ced2faa84fe
jamest1665 <jamest1665@gmail.com>
2026-08-09 12:47:44 -0400

    MainMenu: all 6 maps selectable

--- godot_prototype/scripts/ui/MainMenu.gd
+++ godot_prototype/scripts/ui/MainMenu.gd
@@ -1,13 +1,28 @@
 extends Control
 
 func _ready():
-	print("ForgeFPV MainMenu ready")
+	print("ForgeFPV MainMenu - 7 maps ready")
 
 func _on_donbas():
 	get_tree().change_scene_to_file("res://scenes/maps/DonbasTest.tscn")
 
 func _on_urban():
 	get_tree().change_scene_to_file("res://scenes/maps/UrbanTest.tscn")
+
+func _on_aquatic():
+	get_tree().change_scene_to_file("res://scenes/maps/aquatic_flood/AquaticTest.tscn")
+
+func _on_taiwan():
+	get_tree().change_scene_to_file("res://scenes/maps/global_06_taiwan_littoral/TaiwanTest.tscn")
+
+func _on_arctic():
+	get_tree().change_scene_to_file("res://scenes/maps/arctic/ArcticTest.tscn")
+
+func _on_border():
+	get_tree().change_scene_to_file("res://scenes/maps/border/BorderTest.tscn")
+
+func _on_laport():
+	get_tree().change_scene_to_file("res://scenes/maps/la_port/LAPortTest.tscn")
 
 func _on_quit():
 	get_tree().quit()
